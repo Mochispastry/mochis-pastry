9012157c2101e0be2897537e53719862c2ce2b41
Mochispastry <141682073+Mochispastry@users.noreply.github.com>
2023-09-29 21:06:31 -0300

    Commit Init Auth Service

--- dmaster-basic-service/src/main/java/cl/siatec/dev/sgisystem/dmasterbasicservice/controller/TypeStatusController.java
+++ dmaster-basic-service/src/main/java/cl/siatec/dev/sgisystem/dmasterbasicservice/controller/TypeStatusController.java
@@ -10,7 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/typeStatus")
+@RequestMapping("/typeStatus")
 public class TypeStatusController {
 
     @Autowired
